90de986c0fce1881fc3200e4b8fef84a3741e1ea
KovOlga <switch.to.olga@yandex.ru>
2024-10-28 00:50:54 +0300

    fixed prisma schemas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log('vggg')
+import express, { Application } from "express";
+import prisma from './config/dbclient'
+
+const PORT  = process.env.PORT || 5000;
+const app: Application = express();
+
+const startServer = async () =>{
+    try {
+        await prisma.$connect();
+        console.log('connected')
+        const server = app.listen(PORT, ()=>{
+            console.log(`server is running on port ${PORT}`)
+        })
+    } catch (err){
+        console.log('err')
+    }
+}
+
+startServer()
